a11952ae86bd8a682fa932ec65705d52716abb33
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-07-24 02:21:10 +0530

    added post endpoint

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,13 +1,42 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
+import cors from 'cors';
 console.log('hello');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const client = new PrismaClient();
+app.use(express.json());
+app.use(cors());
+
 // url format: https://hooks.zapier.com/hooks/catch/135136dvzfv/684dfvdzfv
 
-app.get('/health', (req, res) => {
+app.get('/hooks/health', (req, res) => {
   res.status(200).json({ message: 'Hooks server is healthy' });
+});
+
+app.post('/hooks/catch/:userId/:zapId', async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
+
+  await client.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+  res.status(200).json({
+    message: 'Webhook recieved',
+  });
 });
 
 app.listen(PORT, () => {
